369209f02b5f6ea55734a19f650a3690be2e138f
Jubydull <jubydull@gmail.com>
2020-07-08 01:19:52 +0600

    eshop first commit

---
+++ src/main/java/com/jubydull/eshop/eshopdemo/EshopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jubydull.eshop.eshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jubydull/eshop/eshopdemo/EshopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jubydull.eshop.eshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
